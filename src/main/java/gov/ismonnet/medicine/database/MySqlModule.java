6421a743c4b4e5a19e3ca120256a9e585efc6a09
Furrrlo <ferloisbad@gmail.com>
2020-05-08 21:06:24 +0200

    Make MySql service start eagerly to perform db migration

--- src/main/java/gov/ismonnet/medicine/database/MySqlModule.java
+++ src/main/java/gov/ismonnet/medicine/database/MySqlModule.java
@@ -2,7 +2,6 @@ package gov.ismonnet.medicine.database;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
@@ -14,7 +13,7 @@ public class MySqlModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(DatabaseService.class).to(MySqlDatabaseService.class);
-        bind(MySqlDatabaseService.class).in(Singleton.class);
+        bind(MySqlDatabaseService.class).asEagerSingleton();
     }
 
     @Provides
