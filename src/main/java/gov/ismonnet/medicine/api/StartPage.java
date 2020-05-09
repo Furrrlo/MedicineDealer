992bee4733ffc33a700a9047d047ccc934183f9e
Tommaso <tommasointini01@gmail.com>
2020-05-09 12:15:57 +0200

    nothing

--- src/main/java/gov/ismonnet/medicine/api/StartPage.java
+++ src/main/java/gov/ismonnet/medicine/api/StartPage.java
@@ -1,12 +1,17 @@
 package gov.ismonnet.medicine.api;
 
+import gov.ismonnet.medicine.database.Tables;
 import org.jooq.DSLContext;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 
 @Path("pagina_iniziale")
 public class StartPage {
@@ -21,6 +26,16 @@ public class StartPage {
     @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
     public RegistrationBean registrazione(RegistrationBean registrationBean) {
+
+        ctx.insertInto(Tables.UTENTI)
+                .set(Tables.UTENTI.NOME,registrationBean.nome)
+                .set(Tables.UTENTI.COGNOME,registrationBean.cognome)
+                .set(Tables.UTENTI.EMAIL,registrationBean.email)
+                .set(Tables.UTENTI.DATA_NASCITA,new Date(Long.parseLong(registrationBean.dataNascita)))
+                .set(Tables.UTENTI.PASSWORD,registrationBean.password)
+                .execute();
+
+
         return registrationBean;
     }
 
