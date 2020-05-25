6cb6d39d98168f9a90b499b94b17cbdb663e03d0
Furrrlo <ferloisbad@gmail.com>
2020-05-25 13:34:12 +0200

    Fix LocalTime xml converter

--- src/main/java/gov/ismonnet/medicine/converters/LocalTimeXmlAdapter.java
+++ src/main/java/gov/ismonnet/medicine/converters/LocalTimeXmlAdapter.java
@@ -2,8 +2,20 @@ package gov.ismonnet.medicine.converters;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+import static java.time.temporal.ChronoField.*;
 
 public class LocalTimeXmlAdapter extends XmlAdapter<String, LocalTime> {
+
+    private final DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+            .appendValue(HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(MINUTE_OF_HOUR, 2)
+            .appendLiteral(':')
+            .appendValue(SECOND_OF_MINUTE, 2)
+            .toFormatter();
 
     @Override
     public LocalTime unmarshal(String v) {
@@ -12,6 +24,6 @@ public class LocalTimeXmlAdapter extends XmlAdapter<String, LocalTime> {
 
     @Override
     public String marshal(LocalTime v) {
-        return v.toString();
+        return v.format(formatter);
     }
 }
